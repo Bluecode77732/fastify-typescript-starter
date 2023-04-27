46a103049ee4c6b4a03f1063098c309fea938b7c
Bluecode12468 <63056375+Bluecode77732@users.noreply.github.com>
2023-04-27 19:51:22 +0900

    Update userController.ts

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -7,7 +7,7 @@ export default async function userController(fastify: FastifyInstance) {
     reply: FastifyReply
   ) {
     reply.send({
-      balance: "$3,277.32",
+      balance: "$3,277.30",
       picture: "http://placehold.it/32x32",
       age: 30,
       name: "Leonor Cross",
